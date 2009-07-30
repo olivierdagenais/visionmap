4a97b70eb31918f4333957ba74e0107706bd283d
assaflavie <assaflavie@localhost>
2009-07-30 10:37:58 +0000

    Added credentials reading to test

---
+++ projects/FogBugzNet/FogBugzTest.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using FogBugzNet;
+using System.Xml.Serialization;
+
+namespace FogBugzNet
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class FogBugzTest
+    {
+        private Credentials _creds;
+
+
+/*
+ 
+In order to run the test create an XML file with this format:
+ 
+<Credentials>
+	<UserName>yourUserName</UserName>
+	<Password>yourPassword</Password>
+	<Server>http://your-server/FogBugz</Server>
+</Credentials>
+ 
+ */
+        public FogBugzTest()
+        {
+            System.Xml.XmlDocument doc = new System.Xml.XmlDocument();
+            doc.Load("credentials.xml");
+            System.Diagnostics.Trace.Write(doc.InnerXml);
+            _creds = new Credentials();
+            _creds.UserName = doc.SelectSingleNode("//UserName").InnerText;
+            _creds.Password = doc.SelectSingleNode("//Password").InnerText;
+            _creds.Server = doc.SelectSingleNode("//Server").InnerText;
+
+
+
+        }
+
+        private void BadLogin()
+        {
+            FogBugz fb = new FogBugz("bad url");
+            fb.Logon("bad", "bad");
+        }
+
+        [Test]
+        public void TestLogin()
+        {
+            Assert.Throws(typeof (EURLError), new TestDelegate(BadLogin));
+
+            FogBugz fb = new FogBugz(_creds.Server);
+            fb.Logon(_creds.UserName, _creds.Password);
+
+
+        }
+
+
+    }
+}
