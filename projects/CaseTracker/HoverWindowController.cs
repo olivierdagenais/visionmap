9be0d514c08fce059275d7370aa9f82f154857f4
assaflavie <assaflavie@localhost>
2009-08-18 06:59:32 +0000

    FreeMind stuff goes into its own file

--- projects/CaseTracker/HoverWindowController.cs
+++ projects/CaseTracker/HoverWindowController.cs
@@ -464,38 +464,6 @@ namespace FogBugzCaseTracker
 
             return null;
         }
-        private void DoImport()
-        {
-            XmlDocument doc = GetMindMapFromUser();
-            if (doc == null)
-                return;
-
-            Importer imp = new Importer(doc, _fb);
-            ImportAnalysis results = imp.Analyze();
-            if (results.NothingToDo)
-            {
-                MessageBox.Show("No changes were detected. Nothing to import.", "Import Mind Map", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                return;
-            }
-
-            ImportConfirmationDlg dlg = new ImportConfirmationDlg(results);
-
-            if (dlg.ShowDialog() == DialogResult.Yes)
-            {
-                foreach (Case c in results.CaseToNewParent.Keys)
-                {
-                    try
-                    {
-                        _fb.SetParent(c, results.CaseToNewParent[c].ID);
-                    }
-                    catch (Exception x)
-                    {
-                        Utils.LogError(x.ToString());
-                    }
-                }
-            }
-        }
-
 
         private void MoveWindowToCenter()
         {
@@ -577,25 +545,7 @@ namespace FogBugzCaseTracker
                     SetState(new StateRetryLogin(this));
             });
         }
-        private void ExportToFreeMind()
-        {
-            try
-            {
 
-                String tempTabSep = System.IO.Path.GetTempPath() + "cases_" + (Guid.NewGuid()).ToString() + ".mm";
-                // create a writer and open the file
-
-                Exporter ex = new Exporter(_fb, new Search(formatSearch(), _cases));
-                ex.CasesToMindMap().Save(tempTabSep);
-
-                System.Diagnostics.Process.Start("\"" + tempTabSep + "\"");
-            }
-            catch (System.Exception x)
-            {
-                MessageBox.Show("Sorry, couldn't launch Excel");
-                Utils.LogError(x.ToString());
-            }
-        }
 
         private void CloseApplication()
         {
